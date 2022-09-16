fb942c3f1e7097599841dc3649244d21c23abf89
ridedie <ride_die@naver.com>
2022-09-16 23:18:05 +0900

    Class6 Practice movie app

--- Class6/ToDoList/App.js
+++ Class6/ToDoList/App.js
@@ -13,10 +13,13 @@ function App() {
     }
     
     //... 현재 배열 ToDos를 가져와 toDo를 추가하는 역할을 한다. 
-    setToDos(currentArray => [toDo, ...currentArray]); //직전의 값을 받아온다.
+    setToDos(currentArray => [toDo, ...currentArray]); //함수로 직전의 값을 받아온다.
     setToDo("");
   }
 
+  //map은 안에 함수를 넣을 수 있다. 함수는 내가 return한 값은 새로운 배열에 들어간다
+  //하나의 배열에 있는 item을 원하는 무엇이든지 바꿔주는 역할을 한다 그걸 새로운 배열로 return한다, item 자체를 return할 수 있다.
+  //list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어야 한다. 즉 배열을 만들어 각자 고유의 key를 가지게 함
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -24,6 +27,10 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
