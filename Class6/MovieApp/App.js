5886eb035bd2bb970f26de6e8d961c77eb8f617c
ridedie <ride_die@naver.com>
2022-09-19 23:54:52 +0900

    Class6 Practice movie app

--- Class6/MovieApp/App.js
+++ Class6/MovieApp/App.js
@@ -5,6 +5,8 @@ function App() {
   const [loading, setLoading] = useState(true);
   useEffect (() => {
     fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
   }, []);
   return (
     <div>
