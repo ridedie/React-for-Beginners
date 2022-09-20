0fc5751b120d5e35709ae48ddf43685591c4962d
ridedie <ride_die@naver.com>
2022-09-20 23:39:18 +0900

    Class6 Practice movie app

--- Class6/MovieApp/App.js
+++ Class6/MovieApp/App.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-
 function App() {
 
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect (() => {
     fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
     .then((response) => response.json())
