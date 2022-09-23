afe47b196eb977f574d7139df1bcb1544acb4271
ridedie <ride_die@naver.com>
2022-09-23 23:49:23 +0900

    Class6 Practice movie app

---
+++ Class6/MovieApp2/App.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+
+//state로부터 받은 data를 보여준다. (loading, movies)
+//state는 그 data를 API로부터 받아온다. (fetch)
+
+//loading이 되었을 때 setLoading을 false로 바꿔주고
+//movies를 받아오면 API로부터 얻은 data로 State를 변경한다.
+//movies.map을 이용해서 각각의 movie에 접근해서 그 값을 변환할 수 있다.
+function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
+    ).json();
+   
+    setMovies(json.data.movies);
+    setLoading(false);
+    
+  };
+  useEffect (() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  //map은 기존에 있던 배열을 내가 원하는 대로 item이 변형된 새로운 배열을 만들어준다.
+  //map을 사용하기 위해서 key를 꼭 줘야한다.
+  return (
+    <div>
+      {loading ? <h1>Loading...</h1>: movies.map(movie => 
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+        )}
+    </div>
+  );
+}
+
+export default App;
