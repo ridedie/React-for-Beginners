07d477db6f280baa49321ab1c71ca514107f3b4e
ridedie <ride_die@naver.com>
2022-09-30 23:28:50 +0900

    class6 Practice movie app

--- Class6/React_Router/components/Movie.js
+++ Class6/React_Router/components/Movie.js
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
+        //Link는 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트
         <Link to="/movie">{title}</Link>
       </h2>
       <p>{summary}</p>
